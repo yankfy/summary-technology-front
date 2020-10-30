d4a5fd60ff4b5ad9f54cd3506c8ff2705594d662
yankfy <yankfy@qq.com>
2020-10-30 23:33:20 +0800

    fcc middle

---
+++ algorithm/fcc/middle/9.判断一个值是否为布尔原始值.ts
@@ -0,0 +1,10 @@
+function booWho(bool) {
+    // What is the new fad diet for ghost developers? The Boolean.
+    if(bool===false || bool===true){//使用恒等
+        return true;
+    }else{
+        return false;
+    }
+  }
+  
+  booWho(null);
