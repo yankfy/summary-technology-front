a14cbf5b0af92b5a7eedeb68ee927e6eb2a97f10
Pony Yan <yankfy@qq.com>
2020-11-10 17:46:09 +0800

    fcc middle

---
+++ algorithm/fcc/middle/20.检查属性是否为有效值.ts
@@ -0,0 +1,16 @@
+//检查一组对象集合中每个对象的某个属性是否为有效值
+function truthCheck(collection, pre) {
+  return collection.every(function(item){
+  	return Boolean(item[pre])!==false;
+  });
+}
+
+console.log(
+ truthCheck(
+  [
+    {"user": "Tinky-Winky", "sex": "male"},
+    {"user": "Dipsy", "sex": "male"},
+    {"user": "Laa-Laa", "sex": "female"},
+    {"user": "Po", "sex": "female"}
+  ], "sex")
+  );
